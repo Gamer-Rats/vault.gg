cd85460d23f392a7f8ad0d252be1ffab1a028090
Hailey Sampson <167235223+alfie-chu@users.noreply.github.com>
2025-04-16 17:37:04 -0400

    Changed home to index in order to implement firebase and deploy website

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAS6NmXkcY8T-DqzUxLfAVk5ImIiESRSh4",
+    authDomain: "info1601project-f47bb.firebaseapp.com",
+    projectId: "info1601project-f47bb",
+    storageBucket: "info1601project-f47bb.firebasestorage.app",
+    messagingSenderId: "663286137448",
+    appId: "1:663286137448:web:f5fa22dafa73ed611131dd",
+    measurementId: "G-FEJLVGT5TY"
+};
+
+export default firebaseConfig;
